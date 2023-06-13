25f6fec8a5ead0027f1d1ccf9185351253607375
excaliar <langanmatt55@gmail.com>
2023-06-13 14:35:43 -0400

    create 16x16 grid utilizing nested loops in java script and borders/flexbox in css

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//nested loops to create 16x16 grid
+var subContainer = [];
+var row = [] 
+const container = document.querySelector('.container');
+for (let i = 0; i < 16; i++) {
+    subContainer[i] = document.createElement('div')
+    subContainer[i].classList.add('subContainer');
+    container.appendChild(subContainer[i]);
+    for (let j = 0; j < 16; j++) {
+        row[j] = document.createElement('div');
+        row[j].classList.add('row')
+        subContainer[i].appendChild(row[j]);
+    }
+}
